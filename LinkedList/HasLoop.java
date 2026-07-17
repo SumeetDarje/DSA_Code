a7e1fce47b18986304d3a6550e65fd641faabad8
Sumeet Darje <114173649+SumeetDarje@users.noreply.github.com>
2026-07-17 23:42:37 +0530

    Add comments for loop detection algorithm
    Added comments explaining the algorithm for detecting a loop in a linked list using two pointers.

--- LinkedList/HasLoop.java
+++ LinkedList/HasLoop.java
@@ -1,3 +1,21 @@
+// This algorithm uses two pointers: a slow pointer and a fast pointer. The slow pointer moves one step at a time, while the fast pointer moves two steps at a time.
+// If there is a loop in the linked list, the two pointers will eventually meet at some point. If there is no loop, the fast pointer will reach the end of the list.
+// The method should follow these guidelines:
+
+// 1. Create two pointers, slow and fast, both initially pointing to the head of the linked list.
+// 2. Traverse the list with the slow pointer moving one step at a time, while the fast pointer moves two steps at a time.
+// 3. If there is a loop in the list, the fast pointer will eventually meet the slow pointer. If this occurs, the method should return true.
+// 4. If the fast pointer reaches the end of the list or encounters a null value, it means there is no loop in the list. In this case, the method should return false.
+
+// Output:
+// Return true if the linked list has a loop.
+// Return false if the linked list does not have a loop.
+
+// Constraints:
+// You are not allowed to use any additional data structures (such as arrays) or modify the existing data structure.
+// You can only traverse the linked list once.
+
+
 public class LinkedList {
 
     private Node head;
